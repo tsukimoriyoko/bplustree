111f02ba0aa8787516df13b93fe28b7820509dc0
tsukimoriyoko <tsukimoriyoko@yeah.net>
2020-12-06 16:48:33 +0800

    reconsitution

---
+++ draw_distribution.py
@@ -0,0 +1,59 @@
+from itertools import accumulate
+import operator
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import numpy as np
+
+
+l = [[] for i in range(10)]
+max_level = 0
+
+# with open('output1', 'r') as file:
+#     data = file.readlines()
+#     for line in data:
+#         tmp_l = [int(i) for i in line.split(' ')]
+#         cur_level = tmp_l[0]
+#         max_level = max(cur_level, max_level)
+#         for i in range(1, len(tmp_l)):
+#             l[cur_level].append(tmp_l[i])
+
+# for i in range(1, max_level + 1):
+#     l[i] = list(accumulate(l[i], operator.add))
+#     print(len(l[i]), l[i][-1])
+
+
+with open('output', 'r') as file:
+    data = file.readlines()
+    for line in data:
+        max_level += 1
+        l[max_level] = [float(i) for i in line.strip().split(' ')]
+
+# for i in range(max_level + 1):
+#     print(len(l[i]))
+
+m = [[] for i in range(max_level + 1)]
+for i in range(1, max_level + 1):
+    # if (len(l[i]) > 10000):
+    #     t = len(l[i]) // 10000
+    #     l[i] = [l[i][j * t] for j in range(10000)]
+    #     m[i] = [j / 10000 for j in range(10000)]
+    # else:
+    #     m[i] = [j / len(l[i]) for j in range(len(l[i]))]
+
+    m[i] = [(j + 1) / len(l[i]) for j in range(len(l[i]))]
+
+
+l1 = plt.plot(m[1], l[1], color='blue')
+l2 = plt.plot(m[2], l[2], color='green')
+l3 = plt.plot(m[3], l[3], color='yellow')
+l4 = plt.plot(m[4], l[4], color='orange')
+l5 = plt.plot(m[5], l[5], color='red')
+
+plt.xlabel('tree node radio')
+plt.ylabel('access radio')
+
+plt.legend(handles=[l1, l2, l3, l4, l5],
+           labels=['tree level 3', 'tree level 4', 'tree level 5', 'tree level 6', 'tree level 7'])
+
+plt.savefig('5.png')
+plt.show()
---
+++ tests/test_frequency.cpp
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include <queue>
+#include <vector>
+#include <algorithm>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "bplustree.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+using namespace std;
+
+const int FILE_SIZE = 1500000;
+const int MAX_LEVEL = 10;
+
+enum {
+    BPLUS_TREE_LEAF,
+    BPLUS_TREE_NON_LEAF = 1,
+};
+static inline int is_leaf(struct bplus_node *node) {
+    return node->type == BPLUS_TREE_LEAF;
+}
+
+queue<bplus_node*> q_node;
+queue<int> q_level;
+
+vector<int> frequency[MAX_LEVEL];
+
+void dump(bplus_tree *tree) {
+    q_node.push(tree->root);
+    q_level.push(0);
+    while (!q_node.empty()) {
+        bplus_node *node = q_node.front();
+        int level = q_level.front();
+        q_node.pop();
+        q_level.pop();
+        if (is_leaf(node)) {
+            struct bplus_leaf *ln = (struct bplus_leaf *)node;
+            for (int i = 0; i < ln->entries; i++) {
+                frequency[level].push_back(ln->frequency[i]);
+            }
+        } else {
+            struct bplus_non_leaf *nln = (struct bplus_non_leaf *)node;
+            for (int i = 0; i < nln->children; i++) {
+                frequency[level].push_back(nln->frequency[i]);
+                q_node.push(nln->sub_ptr[i]);
+                q_level.push(level + 1);
+            }
+        }
+    }
+    
+    int t_level = tree->level;
+
+    for (int i = 0; i <= t_level; i++) {
+        sort(frequency[i].begin(), frequency[i].end(), greater<int>());
+        // printf("level: %d\n", i);
+        int sum = 0;
+        for (auto j : frequency[i]) {
+            sum += j;
+            printf("%lf ", (double)sum / (double)FILE_SIZE);
+        }
+        // printf("%d\n", frequency[i].size());
+        int t = frequency[i].size();
+        // for (int j = 1; j <= t; j++) {
+        //     printf("%lf ", (double)j / (double)t);
+        // }
+        printf("\n");
+        // printf("sum: %d\n", sum);
+    }
+}
+
+
+struct bplus_tree_config {
+    int order;
+    int entries;
+};
+
+vector<int> input;
+vector<int> run;
+
+int main() {
+    struct bplus_tree *tree;
+    struct bplus_tree_config config;
+    
+    config.entries = config.order = 32;
+    tree = bplus_tree_init(config.order, config.entries);
+    
+    if (tree == NULL) {
+        fprintf(stderr, "Init failure!\n");
+        exit(-1);
+    }
+
+    // freopen("/home/tsukimori/bplustree/data/c_1M5_16_load", "r", stdin);
+    int x, cnt = 0;
+    // for (int i = 0; i < FILE_SIZE; i++) {
+    //     scanf("insert user0000000%d\n", &x);
+    //     bplus_tree_put(tree, x, x);
+    // }
+
+    for (int i = 0; i < FILE_SIZE; i++) {
+        input.push_back(i);
+    }
+    random_shuffle(input.begin(), input.end());
+    for (int i = 0; i < FILE_SIZE; i++) {
+        bplus_tree_put(tree, input[i], input[i]);
+    }
+
+    printf("%d\n", tree->level + 1);
+
+    freopen("/home/tsukimori/bplustree/data/c_1M5_16_run", "r", stdin);
+    // freopen("output_tmp", "w", stdout);
+
+    for (int i = 0; i < FILE_SIZE; i++) {
+        scanf("read user0000000%d\n", &x);
+        run.push_back(x);
+    }
+
+    struct timeval starttime, endtime;
+    double timeuse;
+    gettimeofday(&starttime, NULL);
+
+    for (int i = 0; i < FILE_SIZE; i++) {
+        bplus_tree_get(tree, run[i]);
+        // printf("%d\n", bplus_tree_get(tree, x));
+    }
+    gettimeofday(&endtime, NULL);
+    timeuse = 1000000 * (endtime.tv_sec - starttime.tv_sec) + endtime.tv_usec - starttime.tv_usec;
+
+    printf("%lf ms\n", timeuse / 1000);
+
+    freopen("output", "w", stdout);
+    // dump(tree);
+    // bplus_tree_dump(tree);
+
+    return 0;
+}
